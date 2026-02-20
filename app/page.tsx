84630efd0a124f831cb046df894d00cdf6c6bfec
patycoy <pcoy6742@gmail.com>
2026-02-20 03:55:05 +0000

    Subo los cambios desde el contenedor

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+/*
 import { useAppContext } from '@/context/AppContext';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import Home from '@/components/Home';
@@ -13,3 +13,13 @@ export default function Page() {
     <Home />
   );
 }
+*/
+import Pruebas from '@/components/LandigVistas'; 
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Pruebas />
+    </div>
+  );
+} 
---
+++ components/LandigVistas.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Pruebas(): JSX.Element {            
+    return (    
+        <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-gray-800">
+                ¡Hola, esta es una prueba!
+            </h1>
+        </div>
+    );
+}   
